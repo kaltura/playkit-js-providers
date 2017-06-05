d0d400b70829c098ebfb2e573a5b3a614bd2c1c3
Dan Ziv <dan.ziv@kaltura.com>
2017-06-05 15:27:10 +0300

    chore: update env (#7)

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,13 @@ const webpack = require("webpack");
 const path = require("path");
 const libraryName = "playkit-js-providers";
 
+let plugins = PROD ? [new webpack.optimize.UglifyJsPlugin({sourceMap: true})] : [];
+
 module.exports = {
   context: __dirname + "/src",
   entry: {
-    ovpProvider: "k-provider/ovp/ovp-provider.js",
-    ottProvider: "k-provider/ott/ott-provider.js"
+    "ovpProvider": "k-provider/ovp/ovp-provider.js",
+    "ottProvider": "k-provider/ott/ott-provider.js"
   },
   output: {
     path: path.join(__dirname, "dist"),
@@ -17,24 +19,37 @@ module.exports = {
     libraryTarget: 'umd'
   },
   devtool: 'source-map',
+  plugins: plugins,
   module: {
     rules: [{
       test: /\.js$/,
       use: [{
         loader: "babel-loader"
       }],
-      exclude: [/node_modules/]
+      exclude: [
+        /node_modules/
+      ]
     }, {
       test: /\.js$/,
       exclude: /node_modules/,
       enforce: 'pre',
-      use: [{loader: 'eslint-loader', options: {rules: {semi: 0}}}]
+      use: [{
+        loader: 'eslint-loader',
+        options: {
+          rules: {
+            semi: 0
+          }
+        }
+      }]
     }]
   },
   devServer: {
     contentBase: __dirname + "/src"
   },
   resolve: {
-    modules: [path.resolve(__dirname, "src"), "node_modules"]
+    modules: [
+      path.resolve(__dirname, "src"),
+      "node_modules"
+    ]
   }
 };
