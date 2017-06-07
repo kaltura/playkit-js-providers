8566a6f60b800338d58ecf190488c06ba2983035
Dan Ziv <dan.ziv@kaltura.com>
2017-06-07 10:33:37 +0300

    Update webpack.config.js (#8)

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
 const webpack = require("webpack");
 const path = require("path");
 const libraryName = "playkit-js-providers";
+const PROD = (process.env.NODE_ENV === 'production');
 
 let plugins = PROD ? [new webpack.optimize.UglifyJsPlugin({sourceMap: true})] : [];
 
