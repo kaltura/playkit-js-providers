455536574072879cb7437861332135f6ba3acc09
Oren Me <oren.me@gmail.com>
2017-08-31 15:25:48 +0300

    chore: update dist

--- dist/statsService.js
+++ dist/statsService.js
@@ -268,7 +268,7 @@ Object.defineProperty(exports, "__esModule", {
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _multiRequestBuilder = __webpack_require__(6);
+var _multiRequestBuilder = __webpack_require__(5);
 
 var _multiRequestBuilder2 = _interopRequireDefault(_multiRequestBuilder);
 
@@ -553,7 +553,7 @@ exports.LOG_LEVEL = LOG_LEVEL;
 
 /***/ }),
 
-/***/ 6:
+/***/ 5:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
