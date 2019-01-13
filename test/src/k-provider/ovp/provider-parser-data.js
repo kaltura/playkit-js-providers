49483067273b0cf0252bd2317464bb5aadad5af0
OrenMe <oren.me@gmail.com>
2019-01-13 15:42:13 +0200

    added test

---
+++ test/src/k-provider/ovp/provider-parser-data.js
@@ -0,0 +1,69 @@
+const youtubeMediaEntryData = [
+  '',
+  1740481,
+  null,
+  {
+    entry: {
+      id: '1234',
+      referenceId: 'abcdefg',
+      externalSourceType: 'YouTube',
+      name: 'test youtube entry',
+      description: 'youtube description',
+      dataUrl: 'https://cdnapisec.kaltura.com/p/1111/sp/1111/playManifest/entryId/1234/format/url/protocol/https',
+      type: 'externalMedia.externalMedia',
+      entryType: 1,
+      duration: 0,
+      poster: 'https://cfvod.kaltura.com/p/1111/sp/1111/thumbnail/entry_id/1234/version/100001',
+      tags: ''
+    },
+    playBackContextResult: {
+      hasError: false,
+      data: {
+        sources: [],
+        playbackCaptions: [],
+        flavorAssets: [],
+        actions: [],
+        messages: [],
+        objectType: 'KalturaPlaybackContext'
+      },
+      sources: [],
+      actions: [],
+      messages: [],
+      flavorAssets: []
+    },
+    metadataListResult: {
+      hasError: false,
+      data: {
+        objects: [],
+        totalCount: 0,
+        objectType: 'KalturaMetadataListResponse'
+      },
+      totalCount: 0
+    }
+  }
+];
+
+const youtubeMediaEntryResult = {
+  id: '1234',
+  sources: {
+    progressive: [
+      {
+        id: '1234_youtube',
+        url: 'abcdefg',
+        mimetype: 'video/youtube'
+      }
+    ],
+    dash: [],
+    hls: []
+  },
+  duration: 0,
+  metadata: {
+    description: 'youtube description',
+    name: 'test youtube entry',
+    tags: ''
+  },
+  type: 'Unknown',
+  poster: 'https://cfvod.kaltura.com/p/1111/sp/1111/thumbnail/entry_id/1234/version/100001'
+};
+
+export {youtubeMediaEntryData, youtubeMediaEntryResult};
--- test/src/k-provider/ovp/provider-parser.spec.js
+++ test/src/k-provider/ovp/provider-parser.spec.js
@@ -6,6 +6,7 @@ import {
   kalturaSourceProtocolMismatch,
   kalturaSourceProtocolMismatchFlavorAssets
 } from './playback-sources-data';
+import {youtubeMediaEntryResult, youtubeMediaEntryData} from './provider-parser-data';
 
 describe('provider parser', function() {
   let sandbox;
@@ -32,6 +33,12 @@ describe('provider parser', function() {
       context.flavorAssets = kalturaSourceProtocolMismatchFlavorAssets;
       const adaptiveSource = OVPProviderParser._parseAdaptiveSource(kalturaSourceProtocolMismatch, context, 'myKS', 'myPid', 1234, 1234);
       (adaptiveSource === null).should.be.true;
+    });
+  });
+  describe('getMediaEntry', () => {
+    it('should return a valid youtube source for a valid input', () => {
+      const mediaEntry = OVPProviderParser.getMediaEntry(...youtubeMediaEntryData);
+      mediaEntry.should.deep.equal(youtubeMediaEntryResult);
     });
   });
 });
