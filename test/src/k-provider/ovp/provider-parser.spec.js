43fd1fa13b86e3ea19d11544e0499c415583906f
Shalom Meoded <shalom.meoded@kaltura.com>
2019-11-03 17:21:10 +0200

    chore(FEC-9476): added unit tests for Progressive sources

--- test/src/k-provider/ovp/provider-parser.spec.js
+++ test/src/k-provider/ovp/provider-parser.spec.js
@@ -2,6 +2,9 @@ import OVPProviderParser from '../../../../src/k-provider/ovp/provider-parser';
 import playbackContext from '../../../../src/k-provider/ovp/response-types/kaltura-playback-context';
 import {
   kalturaDashSource,
+  kalturaProgressiveSourceNotSecured,
+  kalturaProgressiveSourceSecured,
+  kalturaProgressive,
   kalturaDashSourceFlavorAssets,
   kalturaSourceProtocolMismatch,
   kalturaSourceProtocolMismatchFlavorAssets
@@ -33,6 +36,38 @@ describe('provider parser', function() {
       context.flavorAssets = kalturaSourceProtocolMismatchFlavorAssets;
       const adaptiveSource = OVPProviderParser._parseAdaptiveSource(kalturaSourceProtocolMismatch, context, 'myKS', 'myPid', 1234, 1234);
       (adaptiveSource === null).should.be.true;
+    });
+  });
+  describe('_parseProgressiveSource', () => {
+    it('should return a valid progressive sources when getting separate http/s', () => {
+      const context = new playbackContext({});
+      context.flavorAssets = kalturaDashSourceFlavorAssets;
+      const progressiveSource = OVPProviderParser._getParsedSources(
+        [kalturaProgressiveSourceNotSecured, kalturaProgressiveSourceSecured],
+        'myKS',
+        1234,
+        1234,
+        {
+          id: '1_938734'
+        },
+        context
+      );
+      progressiveSource.should.exist;
+    });
+    it('should return a valid progressive source for a valid input', () => {
+      const context = new playbackContext({});
+      context.flavorAssets = kalturaDashSourceFlavorAssets;
+      const progressiveSource = OVPProviderParser._getParsedSources(
+        [kalturaProgressive],
+        'myKS',
+        1234,
+        1234,
+        {
+          id: '1_938734'
+        },
+        context
+      );
+      progressiveSource.should.exist;
     });
   });
   describe('getMediaEntry', () => {
