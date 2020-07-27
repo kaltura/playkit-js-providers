0332bb32ebe9815faac34101443d77e50ac4688f
Dan Ziv <dan.ziv@kaltura.com>
2020-07-27 09:30:09 +0300

    update dist

--- src/util/request-builder.js
+++ src/util/request-builder.js
@@ -128,7 +128,7 @@ export default class RequestBuilder {
         }
       }
     };
-    request.open(this.method, this.url, this.retryConfig.async);
+    request.open(this.method, this.url, this.retryConfig && this.retryConfig.async);
     if (this.retryConfig.async && this.retryConfig.timeout) {
       request.timeout = this.retryConfig.timeout;
     }
