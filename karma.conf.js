0f9060833c38caff62a042195d3d2f6a7fc72980
Oren Me <oren.me@gmail.com>
2020-08-30 19:33:37 +0300

    fix webpack config path

--- karma.conf.js
+++ karma.conf.js
@@ -23,7 +23,7 @@ module.exports = function (config) {
     },
     reporters: ['mocha', 'coverage'],
     webpack: {
-      ...require('./webpack.config.js'),
+      ...require('./webpack.config.js')[0],
       externals: {}, //Need to remove externals otherwise they won't be included in test
       devtool: 'inline-source-map', // Need to define inline source maps when using karma
       mode: config.mode || 'development' // run in development mode by default to avoid minifying -> faster
