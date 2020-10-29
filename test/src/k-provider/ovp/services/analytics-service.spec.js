0b317fd35274902de2a898c6c62aef5f96f8c52e
yair.ansbacher <yairans@gmail.com>
2020-10-29 13:25:53 +0200

    feat(FEC-10615): support sending Kava beacons as POST

--- test/src/k-provider/ovp/services/analytics-service.spec.js
+++ test/src/k-provider/ovp/services/analytics-service.spec.js
@@ -3,12 +3,16 @@ import RequestBuilder from '../../../../../src/util/request-builder';
 import OVPConfiguration from '../../../../../src/k-provider/ovp/config';
 import {param} from '../../../../../src/util/param';
 
-describe('analytics service - trackEvent', function () {
+describe.only('analytics service - trackEvent', function () {
   const ovpParams = OVPConfiguration.get();
   const eventParams = {
     param1: 'param1',
     param2: 'param2',
-    param3: 'param3'
+    param3: 'param3',
+    eventType: 1,
+    partnerId: 'partnerId',
+    entryId: 'entryId',
+    sessionId: 'sessionId'
   };
 
   it('should be proper values', function () {
@@ -21,5 +25,38 @@ describe('analytics service - trackEvent', function () {
     request.url.should.be.equal(serviceUrl + '?service=' + request.service + '&action=' + request.action + '&' + param(request.params));
     request.tag.should.be.equal('analytics-trackEvent');
     request.params.should.deep.equal(Object.assign({}, ovpParams.serviceParams, eventParams));
+  });
+
+  it('should be POST request', function () {
+    const serviceUrl = 'http://my/url';
+    const request = OVPAnalyticsService.trackEvent(serviceUrl, eventParams, 'POST');
+    const {param1, param2, param3} = eventParams;
+    const {eventType, partnerId, entryId, sessionId} = eventParams;
+    (request instanceof RequestBuilder).should.be.true;
+    request.service.should.be.equal('analytics');
+    request.action.should.be.equal('trackEvent');
+    request.method.should.be.equal('POST');
+    request.url.should.be.equal(
+      serviceUrl +
+        '?service=' +
+        request.service +
+        '&action=' +
+        request.action +
+        '&' +
+        param({
+          eventType,
+          partnerId,
+          entryId,
+          sessionId
+        })
+    );
+    request.tag.should.be.equal('analytics-trackEvent');
+    request.params.should.deep.equal(JSON.stringify(Object.assign({}, ovpParams.serviceParams, {param1, param2, param3})));
+  });
+
+  it('should be GET request for non POST method', function () {
+    const serviceUrl = 'http://my/url';
+    const request = OVPAnalyticsService.trackEvent(serviceUrl, eventParams, 'BLA');
+    request.method.should.be.equal('GET');
   });
 });
