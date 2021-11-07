f03b68b964bb3e8603795f72589449671eaef97a
JonathanTGold <jonathan.gold@86@gmail.com>
2021-11-07 11:27:51 +0200

    update flow type

--- flow-typed/types/media-config-metadata.js
+++ flow-typed/types/media-config-metadata.js
@@ -1,9 +1,9 @@
 // @flow
 declare type ProviderMediaConfigMetadataObject = {
   name: string,
-  description: string,
-  mediaType: string,
-  metas: Object,
+  description?: string,
+  mediaType?: string,
+  metas?: Object,
   tags: Object,
   epgId?: string,
   recordingId?: string
