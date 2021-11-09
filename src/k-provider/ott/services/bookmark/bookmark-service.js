fa6c6d96b1bf68a0b333fede37fd17d65c4b94c3
JonathanTGold <62672270+JonathanTGold@users.noreply.github.com>
2021-11-09 19:14:18 +0200

    feat(FEC-11611): [Web] Pass the program ID when sending a bookmark (#156)
    Description of the Changes
    reomve the dubug log

--- src/k-provider/ott/services/bookmark/bookmark-service.js
+++ src/k-provider/ott/services/bookmark/bookmark-service.js
@@ -2,18 +2,10 @@
 import OTTService from '../ott-service';
 import RequestBuilder from '../../../../util/request-builder';
 import OTTConfiguration from '../../config';
-import getLogger from '../../../../util/logger';
 
 const SERVICE_NAME: string = 'bookmark';
 
 export default class OTTBookmarkService extends OTTService {
-  /**
-   * The BookmarkService logger
-   * @member {OTTBookmarkService} _logger
-   * @private
-   * @static
-   */
-  static _logger: any = getLogger('BookmarkService');
   /**
    * Creates an instance of RequestBuilder for session.startWidgetSession
    * @function add
@@ -47,7 +39,6 @@ export default class OTTBookmarkService extends OTTService {
       playerData: playerData
     };
     if (bookmark.programId) bookmarkServiceParams.programId = bookmark.programId;
-    this._logger.debug('bookmark added', bookmarkServiceParams);
     const config = OTTConfiguration.get();
     const serviceParams = config.serviceParams;
     Object.assign(serviceParams, {bookmark: bookmarkServiceParams, ks: ks});
