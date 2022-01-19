46c1763b983aa58b8fff4a7490fc3115c458ed5c
Serhii Marchenko <serhii.marchenko@Kaltura.com>
2022-01-19 18:42:57 +0200

    feat(FEV-1164): export ServiceResult

--- src/k-provider/ovp/index.js
+++ src/k-provider/ovp/index.js
@@ -1,6 +1,7 @@
 // @flow
 import OVPProvider from './provider';
 import RequestBuilder from '../../util/request-builder';
+import ServiceResult from '../common/base-service-result';
 import * as ResponseTypes from './response-types';
 
 declare var __VERSION__: string;
@@ -9,4 +10,4 @@ declare var __NAME__: string;
 const NAME = __NAME__ + '-ovp';
 const VERSION = __VERSION__;
 
-export {OVPProvider as Provider, NAME, VERSION, RequestBuilder, ResponseTypes};
+export {OVPProvider as Provider, NAME, VERSION, RequestBuilder, ResponseTypes, ServiceResult};
