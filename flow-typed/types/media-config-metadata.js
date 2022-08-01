973437678030677d4456bb509ce95b593c2c7a7e
Gilad Nadav <gilad.nadav@kaltura.com>
2022-08-01 10:43:41 +0300

    feat(FEC-12414):  add contextType to mediaEntry Metadata & upgrade API version 7.8.1 (#182)
    * add context
    
    * upgrade api version to 7.8.1

--- flow-typed/types/media-config-metadata.js
+++ flow-typed/types/media-config-metadata.js
@@ -3,6 +3,7 @@ declare type ProviderMediaConfigMetadataObject = {
   name: string,
   description?: string,
   mediaType?: string,
+  contextType?: String,
   metas?: Object,
   tags?: Object,
   epgId?: string,
