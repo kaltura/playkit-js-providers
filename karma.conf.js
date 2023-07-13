9d82e4712091fe378b4d90f506986e047a599bc3
JonathanTGold <jonathan.gold@86@gmail.com>
2023-07-13 10:16:02 +0300

    build(FEC-13273): Refactor webpack configuration Remove IE support and reduce bundle size

--- karma.conf.js
+++ karma.conf.js
@@ -1,42 +1,38 @@
-// Create custom launcher in case running with Travis
-const launchers = {
-  Chrome_browser: {
-    base: 'ChromeHeadless',
-    flags: ['--no-sandbox']
-  }
-};
+const webpackConfig = require('./webpack.config')('development', {mode: 'development'});
+delete webpackConfig.entry;
+delete webpackConfig.externals;
+delete webpackConfig.output;
+delete webpackConfig.devServer;
+webpackConfig.devtool = 'inline-source-map';
 
 module.exports = function (config) {
-  let karmaConf = {
-    logLevel: config.LOG_INFO,
-    browsers: [],
-    concurrency: 1,
-    singleRun: true,
-    colors: true,
-    frameworks: ['mocha'],
-    files: ['node_modules/regenerator-runtime/runtime.js', 'test/setup/karma.js'],
+  config.set({
+    frameworks: ['webpack', 'mocha'],
+    files: [
+      {
+        pattern: 'test/setup/karma.js',
+        watched: false
+      }
+    ],
+    exclude: [],
     preprocessors: {
-      'src/**/*.js': ['webpack', 'sourcemap'],
       'test/setup/karma.js': ['webpack', 'sourcemap']
     },
-    reporters: ['mocha', 'coverage'],
-    webpack: {
-      ...require('./webpack.config.js')[0],
-      externals: {}, //Need to remove externals otherwise they won't be included in test
-      devtool: 'inline-source-map', // Need to define inline source maps when using karma
-      mode: config.mode || 'development' // run in development mode by default to avoid minifying -> faster
-    },
-    webpackServer: {
-      noInfo: true
-    },
+    reporters: ['mocha'],
+    webpack: webpackConfig,
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['Chrome', 'Safari', 'Firefox'],
+    singleRun: true,
+    concurrency: Infinity,
+
     client: {
       mocha: {
-        reporter: 'html'
+        reporter: 'html',
+        timeout: 50000
       }
     }
-  };
-
-  karmaConf.customLaunchers = launchers;
-  karmaConf.browsers = ['Chrome_browser'];
-  config.set(karmaConf);
+  });
 };
