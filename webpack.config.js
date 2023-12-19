d01d9ae762381cb94afabb8b0918e9178ccc966d
JonathanTGold <jonathan.gold@86@gmail.com>
2023-12-19 17:49:34 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
 const packageData = require('./package.json');
-const TerserPlugin = require('terser-webpack-plugin');
-
-const isProduction = process.env.NODE_ENV === 'production';
 
 const plugins = [
   new webpack.DefinePlugin({
@@ -12,70 +9,55 @@ const plugins = [
   })
 ];
 
-const config = {
-  entry: {
-    ovp: './src/k-provider/ovp/index.ts',
-    ott: './src/k-provider/ott/index.ts',
-    analytics: './src/k-provider/ovp/services/analytics/index.ts',
-    bookmark: './src/k-provider/ott/services/bookmark/index.ts',
-    stats: './src/k-provider/ovp/services/stats/index.ts'
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: pathData =>
-      pathData.chunk.name === 'ovp' || pathData.chunk.name === 'ott' ? 'playkit-[name]-provider.js' : 'playkit-[name]-service.js',
-    libraryTarget: 'umd',
-    clean: true
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  devtool : 'source-map',
-  plugins: plugins,
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  bugfixes: true,
-                }
+module.exports = (env, {mode}) => {
+  return {
+    entry: {
+      ovp: './src/k-provider/ovp/index.ts',
+      ott: './src/k-provider/ott/index.ts',
+      analytics: './src/k-provider/ovp/services/analytics/index.ts',
+      bookmark: './src/k-provider/ott/services/bookmark/index.ts',
+      stats: './src/k-provider/ovp/services/stats/index.ts'
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: pathData =>
+        pathData.chunk.name === 'ovp' || pathData.chunk.name === 'ott' ? 'playkit-[name]-provider.js' : 'playkit-[name]-service.js',
+      libraryTarget: 'umd',
+      clean: true
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    devtool: mode === 'development' ? 'eval-source-map' : 'source-map',
+    plugins: plugins,
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    bugfixes: true,
+                  }
+                ],
+                '@babel/preset-typescript'
               ],
-              '@babel/preset-typescript'
-            ],
-            plugins: [['@babel/plugin-transform-runtime']]
+              plugins: [['@babel/plugin-transform-runtime']]
+            }
           }
-        }
-      },
-    ]
-  },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        extractComments: false
-      })
-    ]
-  }
-};
-
-module.exports = () => {
-  if (isProduction) {
-    config.mode = 'production';
-  } else {
-    config.mode = 'development';
-    config.devtool = 'eval-source-map';
-    config.devServer = {
+        },
+      ]
+    },
+    devServer: {
       static: {
         directory: path.join(__dirname, 'dist')
       },
       compress: true
-    };
+    }
   }
-  return config;
 };
